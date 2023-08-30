7cbb4c0a83a39815f45bd8ab9381c98158eb2331
Gowtham MA <magowtham7@gmail.com>
2023-08-30 21:31:35 +0530

    connected nodejs to mongodb

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,21 @@
-const express = require('express');
-const PORT = 3000;
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = 5000;
 const app = express();
 
+//mongodb connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/TelephoneDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    throw new Error(`Error occured while connecting to MongoDB  ${err}`);
+  });
 
-app.listen(PORT , ()=>{
-    console.log(`Server Started at ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server Started at ${PORT}`);
+});
