7da28622b115671970a1de95764dae7471be7563
Magowtham <magowtham7@gmail.com>
2023-09-07 12:16:53 +0530

    setupped client part dev

---
+++ Client/admin-panel/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from "./Components/HomePage";
+import Test from "./Components/Test";
+
+function App() {
+  return (
+ <>
+<HomePage/>
+{/* <Test/> */}
+ </>
+  );
+}
+
+export default App;
---
+++ Client/admin-panel/src/Components/HomePage.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../Css/HomePage.css";
+import Test from "./Test";
+import ReactPaginate from "react-paginate";
+function HomePage() {
+  const [data, setData] = useState([]);
+  const handlePageChange = (e) => {
+    console.log(e);
+  };
+  useEffect(() => {
+    async function initialFetch() {
+      try {
+        const data = await axios.get();
+        setData(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        console.log("successfull");
+      }
+    }
+    initialFetch();
+  }, []);
+  return (
+    <>
+      <div className="home-page-container">
+        <nav></nav>
+        <Test data={data} />
+        <ReactPaginate
+          previousLabel={"prev"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          pageCount={30}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={8}
+          onPageChange={handlePageChange}
+          containerClassName={"pagination-container"}
+        />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
