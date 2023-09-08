9233aa8d3e503e6fe1237f45a4f58be8abe43a69
Magowtham <magowtham7@gmail.com>
2023-09-08 15:05:57 +0530

    initial commit

--- Server/Routes/client.js
+++
@@ -1,7 +0,0 @@
-const routes = require("express").Router();
-const userController = require("../controller/user_controller");
-
-routes.post("/adduser", userController.addUser);
-routes.get("/getuser", userController.getUser);
-
-module.exports = routes;
